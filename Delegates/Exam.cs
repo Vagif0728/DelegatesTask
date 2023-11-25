222eb005c14d2493884bbc40344800f4ff448931
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-11-25 23:27:09 +0400

    Task 1 and Task 2 solution

---
+++ Delegates/Exam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Exam
+    {
+        public string Subject { get; set; }
+        public double ExamDuration { get; set; }
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,33 +4,63 @@
     {
         static void Main(string[] args)
         {
-            List<Person> people = new List<Person>
+            //List<Person> people = new List<Person>
+            //{
+            //    new Person { Name = "Vagif", Surname = "Garayev", Age = 19 },
+            //    new Person { Name = "Mata", Surname = "Adigozelova", Age = 20 },
+            //    new Person { Name = "Arif", Surname = "Garayev", Age = 15 },
+            //    new Person { Name = "Nigar", Surname = "Qarayeva", Age = 19 },
+            //     new Person { Name = "Kamil", Surname = "Qarayev", Age = 21 }
+            //};
+
+            //List<Person> kamilInfoList = people.FindAll(p => p.Name == "Kamil");
+            //PrintPeople("Adi Kamil olanlar:", kamilInfoList);
+
+            //List<Person> SurnameList = people.FindAll(p => p.Surname=="ov");
+            //PrintPeople("Soyadi ov ve ova ile biten telebeler:", SurnameList);
+
+            //List<Person> age20List = people.FindAll(p => p.Age >= 20);
+            //PrintPeople("Yawi 20 den cox olanlar:", age20List);
+
+
+
+
+            List<Exam> exams= new List<Exam>
             {
-                new Person { Name = "Vagif", Surname = "Garayev", Age = 19 },
-                new Person { Name = "Mata", Surname = "Adigozelova", Age = 20 },
-                new Person { Name = "Arif", Surname = "Garayev", Age = 15 },
-                new Person { Name = "Nigar", Surname = "Qarayeva", Age = 19 },
-                 new Person { Name = "Kamil", Surname = "Qarayev", Age = 21 }
+                 new Exam { Subject = "Riyaziyyat", ExamDuration = 2, StartDate = DateTime.Now, EndDate = DateTime.Now },
+                 new Exam { Subject = "Fizika", ExamDuration = 3, StartDate = DateTime.Now, EndDate = DateTime.Now }
             };
 
-            List<Person> kamilInfoList = people.FindAll(p => p.Name == "Kamil");
-            PrintPeople("Adi Kamil olanlar:", kamilInfoList);
+            List<Exam> lastWeekExams = exams.FindAll(e => (DateTime.Now - e.StartDate).TotalDays <= 7);
+            PrintExams("Son 1 heftede olan imtahanlar:", lastWeekExams);
 
-            List<Person> SurnameList = people.FindAll(p => p.Surname=="ov");
-            PrintPeople("Soyadi ov ve ova ile biten telebeler:", SurnameList);
+            
+            List<Exam> longExams = exams.FindAll(e => e.ExamDuration > 2);
+            PrintExams("2 saatdan uzun çeken imtahanlar:", longExams);
 
-            List<Person> age20List = people.FindAll(p => p.Age >= 20);
-            PrintPeople("Yawi 20 den cox olanlar:", age20List);
+           
+            List<Exam> onContionsExams = exams.FindAll(e => e.StartDate <= DateTime.Now && e.EndDate > DateTime.Now);
+            PrintExams("Bawlayıb amma bitmemiw imtahanlar:", onContionsExams);
         }
 
-        static void PrintPeople(string message, List<Person> people)
+        static void PrintExams(string add, List<Exam> exams)
         {
-            Console.WriteLine(message);
-            foreach (var person in people)
+            Console.WriteLine(add);
+            foreach (var exam in exams)
             {
-                Console.WriteLine($"Ad: {person.Name}, Soyad: {person.Surname}, Yaş: {person.Age}");
+                Console.WriteLine($"Fenn: {exam.Subject}, Muddeti: {exam.ExamDuration} saat");
             }
            
         }
+
+        //static void PrintPeople(string message, List<Person> people)
+        //{
+        //    Console.WriteLine(message);
+        //    foreach (var person in people)
+        //    {
+        //        Console.WriteLine($"Ad: {person.Name}, Soyad: {person.Surname}, Yaş: {person.Age}");
+        //    }
+
+        //}
     }
 }
